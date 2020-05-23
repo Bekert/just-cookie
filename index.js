87993113744b4c3f4ca37895398a3408f1d22778
Bekert <moroz.bogdan@protonmail.com>
2020-05-23 22:58:49 +0300

    delete object and again add class

--- index.js
+++ index.js
@@ -1,61 +1,5 @@
-// class Cookie {
-//     static get(cookieName) {
-//         if (!document.cookie) {
-//             return console.error('JustCookieError: cookies are not found')
-//         }
-//         const cookieBody = document.cookie.match(new RegExp(`${cookieName}=(.*?)(?:; |$)`))[1]
-//         if (!cookieBody) {
-//             return console.error('JustCookieError: this cookie is not found')
-//         }
-//         return cookieBody
-//     }
-    
-//     static getFull(cookieName) {
-//         if (!document.cookie) {
-//             return console.error('JustCookieError: cookies are not found')
-//         }
-//         const cookie = document.cookie.match(new RegExp(`${cookieName}=(.*?)(?:; |$)`))[0]
-//         if (!cookie) {
-//             return console.error('JustCookieError: this cookie is not found')
-//         }
-//         return cookie
-//     }
-
-//     static getAll() {
-//         if (!document.cookie) {
-//             return console.error('JustCookieError: cookies are not found')
-//         }
-//         return document.cookie
-//     }
-
-//     static set(cookieName, cookieBody) {
-//         document.cookie = `${cookieName}=${cookieBody}`
-//     }
-
-//     static remove(cookieName) {
-//         if (!document.cookie) {
-//             return console.error('JustCookieError: cookies are not found')
-//         }
-//         if (!cookie) {
-//             return console.error('JustCookieError: this cookie is not found')
-//         }
-//         document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`
-//     }
-
-//     static removeAll() {
-//         if (!document.cookie) {
-//             return console.error('JustCookieError: cookies are not found')
-//         }
-//         const cookies = document.cookie.split(';')
-//         for (let cookie of cookies) {
-//             const cookieName = cookie.match(/(.*)=/)[1]
-//             document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`
-//         }
-//     }
-// }
-
-const cookie = {
-    get: function get(cookieName) {
+class Cookie {
+    static get(cookieName) {
         if (!document.cookie) {
             return console.error('JustCookieError: cookies are not found')
         }
@@ -64,9 +8,9 @@ const cookie = {
             return console.error('JustCookieError: this cookie is not found')
         }
         return cookieBody
-    },
-
-    getFull: function getFull(cookieName) {
+    }
+    
+    static getFull(cookieName) {
         if (!document.cookie) {
             return console.error('JustCookieError: cookies are not found')
         }
@@ -75,20 +19,20 @@ const cookie = {
             return console.error('JustCookieError: this cookie is not found')
         }
         return cookie
-    },
+    }
 
-    getAll: function getAll() {
+    static getAll() {
         if (!document.cookie) {
             return console.error('JustCookieError: cookies are not found')
         }
         return document.cookie
-    },
+    }
 
-    set: function set(cookieName, cookieBody) {
+    static set(cookieName, cookieBody) {
         document.cookie = `${cookieName}=${cookieBody}`
-    },
+    }
 
-    remove: function remove(cookieName) {
+    static remove(cookieName) {
         if (!document.cookie) {
             return console.error('JustCookieError: cookies are not found')
         }
@@ -96,9 +40,9 @@ const cookie = {
             return console.error('JustCookieError: this cookie is not found')
         }
         document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`
-    },
+    }
 
-    removeAll: function removeAll() {
+    static removeAll() {
         if (!document.cookie) {
             return console.error('JustCookieError: cookies are not found')
         }
@@ -108,7 +52,6 @@ const cookie = {
             document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`
         }
     }
-
 }
 
 module.exports = cookie
