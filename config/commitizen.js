41462a6771f14dfa94725e7ad84fe99ee3be795c
Bekert <moroz.bogdan@protonmail.com>
2020-05-24 15:52:44 +0300

    build: add commitezen config

---
+++ config/commitizen.js
@@ -0,0 +1,24 @@
+module.exports = {
+  types: [
+    { value: "build", name: "build:     Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)" },
+    { value: "feat", name: "feat:      Add new features, functional" },
+    { value: "fix", name: "fix:       Fix bags" },
+    { value: "refactor", name: "refactor:  Fix code without addition new features" },
+    { value: "docs", name: "docs:      Update docs" },
+    { value: "revert", name: "revert:    Back to previous commits" },
+    { value: "style", name: "style:     Codestile edits (tabs, dots, semicolons etc)" },
+    { value: "ci", name: "build:     Changes to our CI configuration files and scripts (just nothing)" },
+    { value: "test", name: "test:     Add tests" },
+    { value: "chore", name: "chore:     Other changes" },
+    
+  ],
+
+  messages: {
+    type: "Type of your changes:",
+    scope: "\nSelect AREA that you changed (optional):",
+    subject: "Write short DISCRIPTION of your changes:\n",
+    confirmCommit: "Everything okay?"
+  },
+
+  subjectLimit: 72
+};
