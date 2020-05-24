4525439dc38236e79f953716f85b15fd89d7a43e
Bekert <moroz.bogdan@protonmail.com>
2020-05-24 16:31:44 +0300

    style: Format files with prettier

--- config/commitizen.js
+++ config/commitizen.js
@@ -1,26 +1,39 @@
 module.exports = {
-  types: [
-    { value: "build", name: "build:     Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)" },
-    { value: "feat", name: "feat:      Add new features, functional" },
-    { value: "fix", name: "fix:       Fix bags" },
-    { value: "refactor", name: "refactor:  Fix code without addition new features" },
-    { value: "docs", name: "docs:      Update docs" },
-    { value: "revert", name: "revert:    Back to previous commits" },
-    { value: "style", name: "style:     Codestile edits (tabs, dots, semicolons etc)" },
-    { value: "ci", name: "build:     Changes to our CI configuration files and scripts (just nothing)" },
-    { value: "test", name: "test:     Add tests" },
-    { value: "chore", name: "chore:     Other changes" },
-    
-  ],
+    types: [
+        {
+            value: "build",
+            name:
+                "build:     Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
+        },
+        { value: "feat", name: "feat:      Add new features, functional" },
+        { value: "fix", name: "fix:       Fix bags" },
+        {
+            value: "refactor",
+            name: "refactor:  Fix code without addition new features",
+        },
+        { value: "docs", name: "docs:      Update docs" },
+        { value: "revert", name: "revert:    Back to previous commits" },
+        {
+            value: "style",
+            name: "style:     Codestile edits (tabs, dots, semicolons etc)",
+        },
+        {
+            value: "ci",
+            name:
+                "build:     Changes to our CI configuration files and scripts (just nothing)",
+        },
+        { value: "test", name: "test:     Add tests" },
+        { value: "chore", name: "chore:     Other changes" },
+    ],
 
-  messages: {
-    type: "Type of your changes:",
-    subject: "Write short DISCRIPTION of your changes:\n",
-    breaking: "List of BREAKING CHANGES (optional):\n",
-    confirmCommit: "Everything okay?"
-  },
+    messages: {
+        type: "Type of your changes:",
+        subject: "Write short DISCRIPTION of your changes:\n",
+        breaking: "List of BREAKING CHANGES (optional):\n",
+        confirmCommit: "Everything okay?",
+    },
 
-  subjectLimit: 72,
-  allowBreakingChanges: false,
-  upperCaseSubject: true
-};
+    subjectLimit: 72,
+    allowBreakingChanges: false,
+    upperCaseSubject: true,
+}
--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 class Cookie {
     static get(cookieName) {
         if (!document.cookie) {
-            return console.error('JustCookieError: cookies are not found')
+            return console.error("JustCookieError: cookies are not found")
         }
-        const cookieBody = document.cookie.match(new RegExp(`${cookieName}=(.*?)(?:; |$)`))[1]
+        const cookieBody = document.cookie.match(
+            new RegExp(`${cookieName}=(.*?)(?:; |$)`)
+        )[1]
         if (!cookieBody) {
-            return console.error('JustCookieError: this cookie is not found')
+            return console.error("JustCookieError: this cookie is not found")
         }
         return cookieBody
     }
-    
+
     static getFull(cookieName) {
         if (!document.cookie) {
-            return console.error('JustCookieError: cookies are not found')
+            return console.error("JustCookieError: cookies are not found")
         }
-        const cookie = document.cookie.match(new RegExp(`${cookieName}=(.*?)(?:; |$)`))[0]
+        const cookie = document.cookie.match(
+            new RegExp(`${cookieName}=(.*?)(?:; |$)`)
+        )[0]
         if (!cookie) {
-            return console.error('JustCookieError: this cookie is not found')
+            return console.error("JustCookieError: this cookie is not found")
         }
         return cookie
     }
 
     static getAll() {
         if (!document.cookie) {
-            return console.error('JustCookieError: cookies are not found')
+            return console.error("JustCookieError: cookies are not found")
         }
         return document.cookie
     }
@@ -34,19 +38,19 @@ class Cookie {
 
     static remove(cookieName) {
         if (!document.cookie) {
-            return console.error('JustCookieError: cookies are not found')
+            return console.error("JustCookieError: cookies are not found")
         }
         if (!cookie) {
-            return console.error('JustCookieError: this cookie is not found')
+            return console.error("JustCookieError: this cookie is not found")
         }
         document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`
     }
 
     static removeAll() {
         if (!document.cookie) {
-            return console.error('JustCookieError: cookies are not found')
+            return console.error("JustCookieError: cookies are not found")
         }
-        const cookies = document.cookie.split(';')
+        const cookies = document.cookie.split(";")
         for (let cookie of cookies) {
             const cookieName = cookie.match(/(.*)=/)[1]
             document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`
