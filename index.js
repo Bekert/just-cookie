40369f854062e57b9bd9c4f7e3a600dc6b637888
Bekert <moroz.bogdan@protonmail.com>
2020-05-28 19:56:08 +0300

    style: Format all quotes to single

--- index.js
+++ index.js
@@ -4,33 +4,33 @@ const domainRegexp = /(\*?|\.\*?)(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z
 class Cookie {
     static get(cookieName) {
         if (!document.cookie) {
-            return console.error("JustCookieError: cookies are not found")
+            return console.error('JustCookieError: cookies are not found')
         }
         const cookieBody = document.cookie.match(
             new RegExp(`${cookieName}=(.*?)(?:; |$)`)
         )[1]
         if (!cookieBody) {
-            return console.error("JustCookieError: this cookie is not found")
+            return console.error('JustCookieError: this cookie is not found')
         }
         return cookieBody
     }
 
     static getFull(cookieName) {
         if (!document.cookie) {
-            return console.error("JustCookieError: cookies are not found")
+            return console.error('JustCookieError: cookies are not found')
         }
         const cookie = document.cookie.match(
             new RegExp(`${cookieName}=(.*?)(?:; |$)`)
         )[0]
         if (!cookie) {
-            return console.error("JustCookieError: this cookie is not found")
+            return console.error('JustCookieError: this cookie is not found')
         }
         return cookie
     }
 
     static getAll() {
         if (!document.cookie) {
-            return console.error("JustCookieError: cookies are not found")
+            return console.error('JustCookieError: cookies are not found')
         }
         return document.cookie
     }
@@ -42,12 +42,12 @@ class Cookie {
     ) {
         if (this.get(cookieName)) {
             return console.error(
-                "JustCookieError: this cookie already exsist. For rewriting use method setWithRewrite()"
+                'JustCookieError: this cookie already exsist. For rewriting use method setWithRewrite()'
             )
         }
         let cookie = `${cookieName}=${cookieValue}`
 
-        const types = ["path", "domain", "expires", "maxAge", "secure"]
+        const types = ['path', 'domain', 'expires', 'maxAge', 'secure']
 
         const array = [path, domain, expires, maxAge, secure]
 
@@ -55,10 +55,10 @@ class Cookie {
             const value = array[param]
             if (value) {
                 if (+param === 0 && value.search(pathRegexp) === -1) {
-                    return console.error("JustCookieError: wrong path format")
+                    return console.error('JustCookieError: wrong path format')
                 }
                 if (+param === 1 && value.search(domainRegexp) === -1) {
-                    return console.error("JustCookieError: wrong domain format")
+                    return console.error('JustCookieError: wrong domain format')
                 }
 
                 const type = types[param]
@@ -72,7 +72,7 @@ class Cookie {
     static set(cookieName, cookieValue) {
         if (!this.get(cookieName)) {
             return console.error(
-                "JustCookieError: this cookie does not exsist. For adding new cookie use method set()"
+                'JustCookieError: this cookie does not exsist. For adding new cookie use method set()'
             )
         }
         document.cookie = `${cookieName}=${cookieValue}`
@@ -80,19 +80,19 @@ class Cookie {
 
     static remove(cookieName) {
         if (!document.cookie) {
-            return console.error("JustCookieError: cookies are not found")
+            return console.error('JustCookieError: cookies are not found')
         }
         if (!cookie) {
-            return console.error("JustCookieError: this cookie is not found")
+            return console.error('JustCookieError: this cookie is not found')
         }
         document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`
     }
 
     static removeAll() {
         if (!document.cookie) {
-            return console.error("JustCookieError: cookies are not found")
+            return console.error('JustCookieError: cookies are not found')
         }
-        const cookies = document.cookie.split(";")
+        const cookies = document.cookie.split(';')
         for (let cookie of cookies) {
             const cookieName = cookie.match(/(.*)=/)[1]
             document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`
