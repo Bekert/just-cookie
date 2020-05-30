5d9f374bda0b9bea4d58a6739b09feca74da6806
Bekert <moroz.bogdan@protonmail.com>
2020-05-30 16:32:32 +0300

    chore: Compile typescript

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,16 +1,16 @@
 interface CookieParams {
-    path: string;
-    domain: string;
-    expires: Date;
-    maxAge: number;
-    secure: boolean;
+    path?: string;
+    domain?: string;
+    expires?: Date;
+    maxAge?: number;
+    secure?: boolean;
 }
 declare class Cookie {
     static get(cookieName: string): string | void;
     static getFull(cookieName: string): string | void;
     static getAll(): string | void;
-    static add(cookieName: string, cookieValue: string, cookieParams: CookieParams): string | void;
-    static set(cookieName: string, cookieValue: string): void;
+    static add(cookieName: string, cookieValue: string, { path, domain, expires, maxAge, secure }?: CookieParams): void;
+    static set(cookieName: string, cookieValue: string, { path, domain, expires, maxAge, secure }?: CookieParams): void;
     static remove(cookieName: string): void;
     static removeAll(): void;
 }
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var pathRegexp = /\/[^\s]+/gm;
 var domainRegexp = /(\*?|\.\*?)(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/gm;
 var Cookie = /** @class */ (function () {
@@ -9,35 +9,43 @@ var Cookie = /** @class */ (function () {
         if (!document.cookie) {
             return console.error('JustCookieError: cookies are not found');
         }
-        var cookieBody = document.cookie.match(new RegExp(cookieName + "=(.*?)(?:; |$)"))[1];
+        var cookieBody = document.cookie.match(new RegExp(cookieName + "=(.*?)(?:; |$)"));
         if (!cookieBody) {
             return console.error('JustCookieError: this cookie is not found');
         }
-        return cookieBody;
+        return cookieBody[1].toString();
     };
     Cookie.getFull = function (cookieName) {
         if (!document.cookie) {
             return console.error('JustCookieError: cookies are not found');
         }
-        var cookie = document.cookie.match(new RegExp(cookieName + "=(.*?)(?:; |$)"))[0];
+        var cookie = document.cookie.match(new RegExp(cookieName + "=(.*?)(?:; |$)"));
         if (!cookie) {
             return console.error('JustCookieError: this cookie is not found');
         }
-        return cookie;
+        return cookie[0].toString();
     };
     Cookie.getAll = function () {
         if (!document.cookie) {
             return console.error('JustCookieError: cookies are not found');
         }
-        return document.cookie;
+        return document.cookie.toString();
     };
-    Cookie.add = function (cookieName, cookieValue, cookieParams) {
+    Cookie.add = function (cookieName, cookieValue, _a) {
+        var _b = _a === void 0 ? {} : _a, path = _b.path, domain = _b.domain, expires = _b.expires, maxAge = _b.maxAge, secure = _b.secure;
         if (this.get(cookieName)) {
             return console.error('JustCookieError: this cookie already exsist. For rewriting use method setWithRewrite()');
         }
         var cookie = cookieName + "=" + cookieValue;
-        for (var _i = 0, _a = Object.keys(cookieParams); _i < _a.length; _i++) {
-            var type = _a[_i];
+        var cookieParams = {
+            path: path,
+            domain: domain,
+            expires: expires,
+            maxAge: maxAge,
+            secure: secure,
+        };
+        for (var _i = 0, _c = Object.keys(cookieParams); _i < _c.length; _i++) {
+            var type = _c[_i];
             var value = cookieParams[type];
             if (value) {
                 if (type === 'path' &&
@@ -56,21 +64,50 @@ var Cookie = /** @class */ (function () {
                 }
             }
         }
-        return cookie;
+        document.cookie = cookie;
     };
-    Cookie.set = function (cookieName, cookieValue) {
+    Cookie.set = function (cookieName, cookieValue, _a) {
+        var _b = _a === void 0 ? {} : _a, path = _b.path, domain = _b.domain, expires = _b.expires, maxAge = _b.maxAge, secure = _b.secure;
         if (!this.get(cookieName)) {
             return console.error('JustCookieError: this cookie does not exsist. For adding new cookie use method set()');
         }
-        document.cookie = cookieName + "=" + cookieValue;
+        var cookie = cookieName + "=" + cookieValue;
+        var cookieParams = {
+            path: path,
+            domain: domain,
+            expires: expires,
+            maxAge: maxAge,
+            secure: secure,
+        };
+        for (var _i = 0, _c = Object.keys(cookieParams); _i < _c.length; _i++) {
+            var type = _c[_i];
+            var value = cookieParams[type];
+            if (value) {
+                if (type === 'path' &&
+                    value.toString().search(pathRegexp) === -1) {
+                    return console.error('JustCookieError: wrong path format');
+                }
+                if (type === 'domain' &&
+                    value.toString().search(domainRegexp) === -1) {
+                    return console.error('JustCookieError: wrong domain format');
+                }
+                if (type === 'secure') {
+                    cookie = cookie + "; secure";
+                }
+                else {
+                    cookie = cookie + "; " + type + "=" + value;
+                }
+            }
+        }
+        document.cookie = cookie;
     };
     Cookie.remove = function (cookieName) {
         if (!document.cookie) {
             return console.error('JustCookieError: cookies are not found');
         }
-        // if (!cookie) {
-        //     return console.error('JustCookieError: this cookie is not found')
-        // }
+        if (!this.get(cookieName)) {
+            return console.error('JustCookieError: this cookie is not found');
+        }
         document.cookie = cookieName + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     };
     Cookie.removeAll = function () {
@@ -80,10 +117,13 @@ var Cookie = /** @class */ (function () {
         var cookies = document.cookie.split(';');
         for (var _i = 0, cookies_1 = cookies; _i < cookies_1.length; _i++) {
             var cookie = cookies_1[_i];
-            var cookieName = cookie.match(/(.*)=/)[1];
-            document.cookie = cookieName + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            var cookieName = cookie.match(/(.*)=/);
+            if (!cookieName) {
+                return console.error('JustCookieError: some cookie are not found');
+            }
+            document.cookie = cookieName[1] + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
         }
     };
     return Cookie;
 }());
-exports["default"] = Cookie;
+exports.default = Cookie;
